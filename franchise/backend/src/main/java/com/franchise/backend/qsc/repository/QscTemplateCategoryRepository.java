71c0f5b1dd608c34baa39b48eebb3d9a450d641c
jiyoung <jjiyoung0421@gmail.com>
2026-01-29 10:05:11 +0900

    add qsc repository qsctemplateCategoryRepository

---
+++ franchise/backend/src/main/java/com/franchise/backend/qsc/repository/QscTemplateCategoryRepository.java
@@ -0,0 +1,11 @@
+import com.franchise.backend.qsc.entity.QscTemplateCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QscTemplateCategoryRepository
+        extends JpaRepository<QscTemplateCategory, Long> {
+
+    List<QscTemplateCategory>
+    findByTemplate_IdOrderByIdAsc(Long templateId);
+}
