c5f74adcf12e1de5cda2f64e88dba00de834751b
MyHome <rlaalswns199@gmail.com>
2026-02-14 01:57:41 +0900

    Spring SecurityConfig, WebConfig 설정에 도메인 주소 추가

--- franchise/backend/src/main/java/com/franchise/backend/common/config/WebConfig.java
+++ franchise/backend/src/main/java/com/franchise/backend/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedOrigins("https://frimaai.p-e.kr","http://localhost:3000", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
